014d0ad66db07a4d4125574da8dd2122791f485d
moula2002 <iammoulahussain@gmail.com>
2026-07-27 13:11:39 +0530

    new changes

--- src/services/api.js
+++ src/services/api.js
@@ -4,4 +4,17 @@ const api = axios.create({
   baseURL: 'https://bcr-innovations-server-1.onrender.com/api',
 });
 
+// Interceptor to bypass corrupted browser cache
+api.interceptors.request.use((config) => {
+  if (config.method === 'get') {
+    config.params = { ...config.params, _t: Date.now() };
+    config.headers['Cache-Control'] = 'no-cache, no-store, must-revalidate';
+    config.headers['Pragma'] = 'no-cache';
+    config.headers['Expires'] = '0';
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
+});
+
 export default api;
